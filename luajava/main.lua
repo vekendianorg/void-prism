98e032fce122a9ca0aa2c9544c5515b44b3ffe83
lazor-git <zach.legend.2nd@gmail.com>
2026-06-30 23:56:35 +0700

    Initial Prism repository

--- luajava/main.lua
+++ luajava/main.lua
@@ -9,9 +9,13 @@ Prism.Crypto = luajava.bindClass("org.vekendian.Crypto")
 
 -- Paths
 Prism.Paths = {}
-Prism.Paths.CONTENT = "/data/user/0/com.waxmoon.ma.gp/rootfs/data/user/0/com.fingersoft.hcr2/files/content_cache/",
+Prism.Paths.CONTENT = "/data/user/0/com.waxmoon.ma.gp/rootfs/data/user/0/com.fingersoft.hcr2/files/content_cache/"
 Prism.Paths.PACKAGES = Prism.Paths.CONTENT .. "packages/"
 Prism.Paths.INDEX = Prism.Paths.PACKAGES .. ".packages"
+Prism.Paths.JSON = Prism.Paths.CONTENT .. "json/"
+Prism.Paths.EVENTS = Prism.Paths.JSON .. "events/"
+Prism.Paths.SEASONS = Prism.Paths.JSON .. "seasons/"
+Prism.Paths.SHOP = Prism.Paths.JSON .. "shop/"
 
 -- Modules
 Prism.Loader = require("loader.lua")
@@ -23,3 +27,6 @@ Prism.Loader.init(Prism)
 Prism.Builder.init(Prism)
 Prism.Validator.init(Prism)
 
+
+
+return Prism
